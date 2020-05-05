a2d086a785a9ba2a33d76fadcc7cab69ae6ce65c
cristian98st <44675214+cristian98st@users.noreply.github.com>
2020-05-05 13:38:43 +0300

    Laborator 9

---
+++ Laborator 9/Asp_Tugui_Stefan_Rp/Asp_Tugui_Stefan_Rp/Data/Asp_Tugui_Stefan_RpContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Asp_Tugui_Stefan_Rp.Models;
+
+namespace Asp_Tugui_Stefan_Rp.Data
+{
+    public class Asp_Tugui_Stefan_RpContext : DbContext
+    {
+        public Asp_Tugui_Stefan_RpContext (DbContextOptions<Asp_Tugui_Stefan_RpContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Asp_Tugui_Stefan_Rp.Models.Movie> Movie { get; set; }
+    }
+}
